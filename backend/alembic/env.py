4555c4608fa76c54fdbd43465115cc1af326346c
snehasivadas21 <snehasivadas2002@gmail.com>
2026-09-06 22:50:09 +0530

    implement user profile api

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,6 +11,7 @@ from app.models.users import User
 from app.models.email_verification import EmailVerification
 from app.models.refresh_token import RefreshToken
 from app.models.password_reset_token import PasswordResetToken
+from app.models.user_profile import UserProfile
 
 from app.db.session import settings
 
